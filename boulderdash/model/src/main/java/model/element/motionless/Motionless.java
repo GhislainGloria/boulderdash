f5ce71fee346ddbf3c7301984d00b38c07a2a3cd
GhislainGloria <ghislain.gloria@viacesi.fr>
2017-06-18 18:02:12 +0200

    Phase 1 : Code Cap

--- boulderdash/model/src/main/java/model/element/motionless/Motionless.java
+++ boulderdash/model/src/main/java/model/element/motionless/Motionless.java
@@ -3,12 +3,13 @@ package model.element.motionless;
 import model.element.Position;
 
 /**
- * <h1>The class define what a motionless element is.</h1>
+ * <h1>The abstract class Motionless define what a motionless element is.</h1>
  *
  * @author Ghislain Gloria
  * @version 1.0
  */
 
 public abstract class Motionless {
+	private int ID;
 	private Position position;
 }
