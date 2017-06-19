63a1788a5ae9c99183a019bf1ef27256f5bcf154
X240_W10_64 <X240_W10_64@10.192.128.6>
2017-06-19 15:15:57 +0200

    Phase 0

---
+++ boulderdash/controller/src/main/java/controller/ControllerBoulder.java
@@ -0,0 +1,152 @@
+package controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import fr.exia.insanevehicles.controller.IOrderPerformer;
+import fr.exia.insanevehicles.controller.UserOrder;
+import fr.exia.insanevehicles.model.IInsaneVehiclesModel;
+import fr.exia.insanevehicles.view.IInsaneVehiclesView;
+import model.Example;
+import model.IModel;
+import view.IView;
+
+/**
+ * <h1>The Class ControllerBoulder.</h1>
+ *
+ * @author chloe
+ * @version 1.0
+ */
+public class ControllerBoulder implements IController, IOrderPerformer {
+
+	/** The view. */
+	private final IView view;
+
+	/** The model. */
+	private final IModel model;
+
+	/** The speed. */
+	private final int speed = 300;
+
+	/** The stack order. */
+	private UserOrder stackOrder;
+
+	/**
+	 * Instantiates a new controller.
+	 *
+	 * @param view
+	 *            the view
+	 * @param model
+	 *            the model
+	 */
+	public ControllerBoulder(final IView view, final IModel model) {
+		super();
+		this.view = view;
+		this.model = model;
+		this.clearStackOrder();
+	}
+
+	/**
+	 * Start the game.
+	 *
+	 * @throws Exception
+	 *
+	 */
+	public void start() throws InterruptedException {
+		while (this.getModel().getMyCharacter().isAlive()) {
+			Thread.sleep(speed);
+			switch (this.getStackOrder()) {
+			case RIGHT:
+				this.getModel().getMyCharacter().moveRight();
+				break;
+			case LEFT:
+				this.getModel().getMyCharacter().moveLeft();
+				break;
+			case UP:
+				this.getModel().getMyCharacter().moveUp();
+				break;
+			case DOWN:
+				this.getModel().getMyCharacter().moveDown();
+				break;
+			case NOP:
+			default:
+				this.getModel().getMyCharacter().doNothing();
+				break;
+			}
+			this.clearStackOrder();
+			if (this.getModel().getMyCharacter().isAlive()) {
+
+			}
+		}
+		this.getView().displayMessage("Game over! You are dead!");
+	}
+
+	/**
+	 * Gets the view.
+	 *
+	 * @return the view
+	 */
+	public IView getView() {
+		return this.view;
+	}
+
+	/**
+	 * Sets the view.
+	 *
+	 * @param view
+	 *            the view to set
+	 */
+	private void setView(final IView view) {
+		this.view = view;
+	}
+
+	/**
+	 * Gets the model.
+	 *
+	 * @return the model
+	 */
+	public IModel getModel() {
+		return this.model;
+	}
+
+	/**
+	 * Sets the model.
+	 *
+	 * @param model
+	 *            the model to set
+	 */
+	private void setModel(final IModel model) {
+		this.model = model;
+	}
+
+	/**
+	 * Gets the stack order.
+	 *
+	 * @return the stack order
+	 */
+	private UserOrder getStackOrder() {
+		return this.stackOrder;
+	}
+
+	/**
+	 * Sets the stack order.
+	 *
+	 * @param stackOrder
+	 *            the new stack order
+	 */
+	private void setStackOrder(final UserOrder stackOrder) {
+		this.stackOrder = stackOrder;
+	}
+
+	/**
+	 * Clear stack order.
+	 */
+	private void clearStackOrder() {
+		this.stackOrder = UserOrder.NOP;
+	}
+
+	@Override
+	public IOrderPerformer getOrderPeformer() {
+		return this;
+	}
+}
