cfdccf4c19702718aa673f1ce039959dc0e9ddb2
kayliechloe <chlochlocc@gmail.com>
2017-06-23 23:32:07 +0200

    Test Controller

---
+++ test/boulderdashtrue/ControllerBoulderTest.java
@@ -0,0 +1,14 @@
+package boulderdashtrue;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControllerBoulderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
